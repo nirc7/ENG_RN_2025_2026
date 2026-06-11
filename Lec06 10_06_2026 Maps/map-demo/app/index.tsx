86444cef74278b2f786721c33ab82716070de37b
nc <31476486+nirc7@users.noreply.github.com>
2026-06-11 10:08:38 +0300

    maps

--- Lec06 10_06_2026 Maps/map-demo/app/index.tsx
+++ Lec06 10_06_2026 Maps/map-demo/app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View , Dimensions} from "react-native";
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import { Text, View, Dimensions } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function Index() {
   return (
@@ -11,9 +11,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Ruppin.</Text>
+      <Text> this screen.</Text>
+      {/* <MapView style={{width:'90%', height:'80%'}} /> */}
       <MapView
-        style={{ flex: 0.7, width: Dimensions.get('window').width - 30, }}
+        style={{ flex: 0.7, width: Dimensions.get('window').width - 40, }}
         region={{
           latitude: 32.157154,
           longitude: 34.843893,
@@ -23,10 +24,19 @@ export default function Index() {
         <Marker
           coordinate={{
             latitude: 32.15715,
-            longitude: 34.843893
+            longitude: 34.8437893
           }}
           title='my place:)'
           description='here i am'
+        //image={require('../assets/icon.png')}
+        />
+         <Marker
+          coordinate={{
+            latitude: 32.15715,
+            longitude: 34.8477893
+          }}
+          title='my place:)2'
+          description='here i am2'
         //image={require('../assets/icon.png')}
         />
       </MapView>
